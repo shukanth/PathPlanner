c28371aeeebdfcf372226b8dfdf8151fa517bf7f
nithinseelan <nithindseelan@gmail.com>
2024-10-19 17:16:45 -0400

    Home page completed

--- frontend/src/landingPage.js
+++ frontend/src/landingPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css';
 
 function App1() {
   return (
@@ -8,7 +9,7 @@ function App1() {
         <nav className="nav">
           <div className="dropdown">
             <button className="dropdown-button">
-              Home <svg width="8" height="8" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg"><path d="M0 3L5 8L10 3H0Z" fill="#fff"/></svg>
+              Home <svg width="8" height="8" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg"><path d="M0 3L5 8L10 3H0Z" fill="#fff" /></svg>
             </button>
             <div className="dropdown-content">
               <a href="#select-major">Select Major</a>
@@ -26,8 +27,11 @@ function App1() {
       </header>
       {/* Main Content */}
       <div className="content">
-        <div className="main-content">Start your degree early</div>
-        <div className="side-content">Text about Path Planner</div>
+        <div className="main-content"><img style={{ width: '650px', height: '390px' }} src="/ui.JPG" alt="" /></div>
+        <div className="side-content">
+          <b style={{ fontSize: '25px', marginBottom: '35px' }}>Start your degree early!</b>
+          <div>Your journey to college starts here! At College Path Planner, we help high school students map out their academic schedules to ensure they meet college admissions requirements with ease. Whether you’re aiming for a top-tier university or just want to be prepared for what’s next, our platform simplifies the process.</div>
+        </div>
       </div>
       {/* Get Started Button */}
       <div className="get-started">
